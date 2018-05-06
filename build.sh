9219728fa07a195e841229d078de4dd2cde4c426
Gene McCulley <mcculley@stackframe.com>
2018-05-06 13:24:24 -0400

    Initial import.

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+mkdir output
+
+set -e
+
+for militiaMember in militia_members/*
+do
+  militiaMember=$(basename $militiaMember)
+  xsltproc template.xml militia_members/$militiaMember/info.xml > militia_members/$militiaMember/$militiaMember.html &
+done
+
+wait
+
+for militiaMember in militia_members/*
+do
+  militiaMember=$(basename $militiaMember)
+  wkhtmltopdf --dpi 380 --page-size letter militia_members/$militiaMember/$militiaMember.html output/$militiaMember.pdf &
+  wkhtmltoimage militia_members/$militiaMember/$militiaMember.html output/$militiaMember.png &
+done
+
+wait
